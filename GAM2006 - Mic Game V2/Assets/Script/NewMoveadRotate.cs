95be65f2189d24a0df5e069648204cb3fb0e0064
MarioMingoia <MM3417@live.mdx.ac.uk>
2020-10-07 18:30:26 +0100

    Modification to rotation

--- GAM2006 - Mic Game V2/Assets/Script/NewMoveadRotate.cs
+++ GAM2006 - Mic Game V2/Assets/Script/NewMoveadRotate.cs
@@ -13,12 +13,15 @@ public class NewMoveadRotate : MonoBehaviour
     private Vector3 V3_move_direction = Vector3.zero;
     public static CharacterController cc_Reference_To_Character_Controller;
 
+    private Vector3 targetRot;
+
     // Start is called before the first frame update
     void Start()
     {
         cc_Reference_To_Character_Controller = GetComponent<CharacterController>();
         speed = fl_MovementSpeed;
 
+        targetRot = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -37,6 +40,23 @@ public class NewMoveadRotate : MonoBehaviour
             V3_move_direction.y -= fl_gravity * Time.deltaTime;
 
         }
+      /*  if (Input.GetAxis("Horizontal") < 0)
+        {
+            targetRot.y = targetRot.y - 90;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            targetRot.y = targetRot.y + 90;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            targetRot.y = targetRot.y + 180;
+        }
+        if (targetRot.y == 360 || targetRot.y == -360)
+        {
+            targetRot.y = 0;
+        }
+        transform.eulerAngles = targetRot;*/
 
         cc_Reference_To_Character_Controller.Move(V3_move_direction);
 
@@ -45,7 +65,7 @@ public class NewMoveadRotate : MonoBehaviour
     {
         Vector3 _temp_direction = Vector3.zero;
 
-        _temp_direction.x = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+       // _temp_direction.x = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
         _temp_direction.z = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
         V3_move_direction = gameObject.transform.TransformDirection(_temp_direction);
     }
