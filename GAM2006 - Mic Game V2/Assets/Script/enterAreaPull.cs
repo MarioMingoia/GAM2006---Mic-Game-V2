04b0993ef8af90b8b72343e5a9fb4828122a7520
MarioMingoia <MM3417@live.mdx.ac.uk>
2020-10-13 11:56:50 +0100

    Camera
    Fixed the camera changes between the pull area and the rest of the game. Done by changing the camera variables to game objects

---
+++ GAM2006 - Mic Game V2/Assets/Script/enterAreaPull.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterAreaPull : MonoBehaviour
+{
+
+    public GameObject MainCam;
+    public GameObject pullCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCam = GameObject.Find("MainCamera");
+        pullCam = GameObject.Find("Pullcamera");
+        pullCam.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "2ndCam")
+        {
+            MainCam.gameObject.SetActive(false);
+            pullCam.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "2ndCam")
+        {
+            MainCam.gameObject.SetActive(true);
+            pullCam.gameObject.SetActive(false);
+        }
+    }
+}
